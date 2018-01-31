e863c004e2e0d75ecb453f94cef3234f646e7ec4
Simon <simon.wolko@gmail.com>
2018-01-31 17:59:44 +0100

    Init repo

---
+++ src/main/java/com/biobelt/biobeltapi/BiobeltapiApplication.java
@@ -0,0 +1,12 @@
+package com.biobelt.biobeltapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiobeltapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiobeltapiApplication.class, args);
+	}
+}
